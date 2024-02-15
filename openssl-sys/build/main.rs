59678ddfc58095645edbb59da4703ed14376b33e
Zhang Jingqiang <zh_jq@outlook.com>
2024-02-15 15:04:20 +0800

    update to aws-lc-sys 0.13.1

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -68,7 +68,7 @@ fn find_openssl(target: &str) -> (Vec<PathBuf>, PathBuf) {
     }
     #[cfg(feature = "aws-lc")]
     {
-        if let Ok(dir) = env::var("DEP_AWS_LC_0_13_0_INCLUDE") {
+        if let Ok(dir) = env::var("DEP_AWS_LC_0_13_1_INCLUDE") {
             return (Vec::new(), PathBuf::from(dir));
         }
     }
